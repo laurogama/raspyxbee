6ee3557faf1eb3a34ab27c5b425c8d3be59b85c3
Lauro Gama <lauro.gama@fpf.br>
2014-11-22 19:31:11 -0400

    route of the router: routeception

--- rest.py
+++ rest.py
@@ -4,11 +4,45 @@ from flask.ext import restful
 app = Flask(__name__)
 api = restful.Api(app)
 
-endpoints = [{'name': 'tomada1',
-              'actions': [{'command': 'ligar', 'description': 'turns the power outlet ON',
-                           'example': '/api/endpoints/tomada1/ligar', },
-                          {'command': 'desligar', 'description': 'turns the power outlet OFF'}]},
-             {'name': 'tomada2', 'actions': []}]
+endpoints = [
+    {
+        'name': 'tomada1',
+        'xbee_id': '40ABBC08',
+        'actions': [{'command': 'ligar', 'description': 'turns the power outlet ON',
+                     'example': '/api/endpoints/tomada1/ligar', },
+                    {'command': 'desligar', 'description': 'turns the power outlet OFF'}]
+    },
+    {
+        'name': 'tomada2',
+        'xbee_id': '40ABBA21',
+        'actions': [
+            {'command': 'ligar', 'description': 'turns the power outlet ON',
+             'example': '/api/endpoints/tomada2/ligar', },
+            {'command': 'desligar', 'description': 'turns the power outlet OFF'}
+        ]
+    },
+    {
+        'name': 'tomada3',
+        'xbee_id': '40ABBB4E',
+        'actions': [
+            {'command': 'ligar', 'description': 'turns the power outlet ON',
+             'example': '/api/endpoints/tomada3/ligar', },
+            {'command': 'desligar', 'description': 'turns the power outlet OFF'}
+        ]
+    },
+    {
+        'name': 'infrared1',
+        'xbee_id': '40ABB842',
+        'actions': [
+        ]
+    },
+    {
+        'name': 'infrared2',
+        'xbee_id': '40ABBA0B',
+        'actions': [
+        ]
+    }
+]
 
 
 @app.route("/")
@@ -16,8 +50,9 @@ def hello():
     return "Snakebed! A REST API for iteracting with IOT devices using Flask."
 
 
-def execute_action(id, action):
-    return "execute action: " + action + " on device: " + id
+def execute_action(endpoint, action):
+    # TODO implement backend calls
+    return "execute action: " + action + " on device: " + endpoint['name']
 
 
 class Endpoints(restful.Resource):
@@ -28,14 +63,21 @@ class Endpoints(restful.Resource):
                     if action is not None:
                         for act in endpoint.get('actions'):
                             if act.get('command') == action:
-                                return execute_action(id, action)
+                                return execute_action(endpoint, action)
                     return endpoint
         return endpoints
+
+
+class Router(restful.Resource):
+    def get(self):
+        return {'xbee_id': '40ABBB4E'}
 
 
 api.add_resource(Endpoints, '/api/endpoints', '/api/endpoints/<string:id>',
                  '/api/endpoints/<string:id>/<string:action>',
                  endpoint='get')
+
+api.add_resource(Router, '/api/router')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
