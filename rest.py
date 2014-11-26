617fc5fd5b8475db428d888beac52fb22f91b315
Lauro Gama <lauro.gama@fpf.br>
2014-11-26 17:10:43 -0400

    working prototype

--- rest.py
+++ rest.py
@@ -1,6 +1,9 @@
 from flask import Flask
 from flask.ext import restful
 
+from commandHandler import EndpointHandler
+
+
 app = Flask(__name__)
 api = restful.Api(app)
 
@@ -34,12 +37,16 @@ endpoints = [
         'name': 'infrared1',
         'xbee_id': '40ABB842',
         'actions': [
-        ]
-    },
-    {
-        'name': 'infrared2',
-        'xbee_id': '40ABBA0B',
-        'actions': [
+            {'command': 'power', 'description': 'turns the power ON or OFF',
+             'example': '/api/endpoints/infrared1/power', },
+            {'command': 'canalup', 'description': 'increases the channel',
+             'example': '/api/endpoints/infrared1/ligar', },
+            {'command': 'canaldown', 'description': 'decreases the channel',
+             'example': '/api/endpoints/infrared1/canalup', },
+            {'command': 'volumeup', 'description': 'turns the volume UP',
+             'example': '/api/endpoints/infrared1/canaldown', },
+            {'command': 'volumedown', 'description': 'turns the volume DOWN',
+             'example': '/api/endpoints/infrared1/volumeup', },
         ]
     }
 ]
@@ -50,11 +57,6 @@ def hello():
     return "Snakebed! A REST API for iteracting with IOT devices using Flask."
 
 
-def execute_action(endpoint, action):
-    # TODO implement backend calls
-    return "execute action: " + action + " on device: " + endpoint['name']
-
-
 class Endpoint(restful.Resource):
     def get(self, id=None, action=None):
         if id is not None:
@@ -63,7 +65,8 @@ class Endpoint(restful.Resource):
                     if action is not None:
                         for act in endpoint.get('actions'):
                             if act.get('command') == action:
-                                return execute_action(endpoint, action)
+                                # print endpoint['xbee_id']
+                                return EndpointHandler.send_message(endpoint['xbee_id'], action)
                     return endpoint
         return endpoints
 
